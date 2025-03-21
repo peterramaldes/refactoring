a50f391b9f8b97b27cfbcb2febdef857530a6dc8
Peter Admilson Ramaldes <peter.ramaldes@gmail.com>
2025-03-21 07:36:48 -0300

    Change Function Declaration

--- cap1/01-first-example/index.js
+++ cap1/01-first-example/index.js
@@ -12,6 +12,28 @@ function statement(invoice, plays) {
     return plays[aPerformance.playID];
   }
 
+  function amountFor(aPerformance) {
+    let result = 0;
+    switch (playFor(aPerformance).type) {
+      case "tragedy":
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30)
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${playFor(aPerformance).type}`)
+    }
+    return result;
+  }
+
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`
@@ -19,7 +41,7 @@ function statement(invoice, plays) {
     { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format;
 
   for (let perf of invoice.performances) {
-    let thisAmount = amountFor(perf, playFor(perf));
+    let thisAmount = amountFor(perf);
 
     // Sum credits per volume
     volumeCredits += Math.max(perf.audience - 30, 0)
@@ -37,27 +59,7 @@ function statement(invoice, plays) {
   return result;
 }
 
-function amountFor(aPerformance, play) {
-  let result = 0;
-  switch (play.type) {
-    case "tragedy":
-      result = 40000;
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30)
-      }
-      break;
-    case "comedy":
-      result = 30000;
-      if (aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience - 20);
-      }
-      result += 300 * aPerformance.audience;
-      break;
-    default:
-      throw new Error(`unknown type: ${play.type}`)
-  }
-  return result;
-}
+
 
 
 
