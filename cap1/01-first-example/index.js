50850b857c96cda170376dd9d0741c18c56ad8ee
Peter Admilson Ramaldes <peter.ramaldes@gmail.com>
2025-03-22 07:20:36 -0300

    Extract Function (volumeCreditsFor)

--- cap1/01-first-example/index.js
+++ cap1/01-first-example/index.js
@@ -15,11 +15,7 @@ function statement(invoice, plays) {
     { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format;
 
   for (let perf of invoice.performances) {
-    // Sum credits per volume
-    volumeCredits += Math.max(perf.audience - 30, 0)
-
-    // Sum extra credit each comedy audience
-    if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+    volumeCredits += volumeCreditsFor(perf);
 
     // Show the line for this request
     result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
@@ -53,6 +49,14 @@ function statement(invoice, plays) {
       default:
         throw new Error(`unknown type: ${playFor(aPerformance).type}`)
     }
+    return result;
+  }
+
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    if ("comedy" === playFor(aPerformance).type)
+      result += Math.floor(aPerformance.audience / 5);
     return result;
   }
 }
