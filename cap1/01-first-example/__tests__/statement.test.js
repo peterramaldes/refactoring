d8476176ecd69cd1be7d8cbd3a477945e02f4c8e
Peter Admilson Ramaldes <peter.ramaldes@gmail.com>
2025-03-22 08:37:58 -0300

    Rendering HTML

--- cap1/01-first-example/__tests__/statement.test.js
+++ cap1/01-first-example/__tests__/statement.test.js
@@ -1,8 +1,8 @@
-const statement = require('../statement');
+const { statement, htmlStatement } = require('../statement');
 
 const normalize = (str) => str.replace(/\s+/g, ' ').trim();
 
-test('statement', () => {
+test('plainText', () => {
   const invoices = require('../invoices.json');
   const plays = require('../plays.json');
   const result = statement(invoices[0], plays);
@@ -14,6 +14,42 @@ test('statement', () => {
    Othello: $500.00 (40 seats)
   Amount owned is $1,730.00
   You earned 47 credits
+  `;
+
+  expect(normalize(result)).toEqual(normalize(expectedResult));
+})
+
+test('html', () => {
+  const invoices = require('../invoices.json');
+  const plays = require('../plays.json');
+  const result = htmlStatement(invoices[0], plays);
+
+  const expectedResult = `
+  <h1>Statement for BigCo</h1>
+  <table>
+    <tr>
+      <th>Play</th>
+      <th>Seats</th>
+      <th>Cost</th>
+    </tr>
+    <tr>
+      <td>Hamlet</td>
+      <td>55</td>
+      <td>$650.00</td>
+    </tr>
+    <tr>
+      <td>As You Like It</td>
+      <td>35</td>
+      <td>$580.00</td>
+    </tr>
+    <tr>
+      <td>Othello</td>
+      <td>40</td>
+      <td>$500.00</td>
+    </tr>
+  </table>
+  <p>Amount owed is <em>$1,730.00</em></p>
+  <p>You earned <em>47</em> credits</p>
   `;
 
   expect(normalize(result)).toEqual(normalize(expectedResult));
--- cap1/01-first-example/statement.js
+++ cap1/01-first-example/statement.js
@@ -14,13 +14,47 @@ function renderPlainText(data) {
   result += `Amount owned is ${usd(data.totalAmount)}\n`;
   result += `You earned ${data.totalVolumeCredits} credits\n`;
   return result;
+}
 
-  function usd(aNumber) {
-    return new Intl.NumberFormat(
-      "en-US",
-      { style: "currency", currency: "USD", minimumFractionDigits: 2 }
-    ).format(aNumber / 100);
+function htmlStatement(invoice, plays) {
+  return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
+  return `
+  <h1>Statement for BigCo</h1>
+  <table>
+    <tr>
+      <th>Play</th>
+      <th>Seats</th>
+      <th>Cost</th>
+    </tr>
+    ${renderPerformancesHtml(data.performances)}
+  </table>
+  <p>Amount owed is <em>${usd(data.totalAmount)}</em></p>
+  <p>You earned <em>${data.totalVolumeCredits}</em> credits</p>
+  `
+
+  function renderPerformancesHtml(performances) {
+    let result = "";
+    for (let perf of performances) {
+      result += `
+      <tr>
+        <td>${perf.play.name}</td>
+        <td>${perf.audience}</td>
+        <td>${usd(perf.amount)}</td>
+      </tr>  
+      `
+    }
+    return result;
   }
 }
 
-module.exports = statement;
+function usd(aNumber) {
+  return new Intl.NumberFormat(
+    "en-US",
+    { style: "currency", currency: "USD", minimumFractionDigits: 2 }
+  ).format(aNumber / 100);
+}
+
+module.exports = { statement, htmlStatement };
