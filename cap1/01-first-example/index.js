31d6e003f96a2f6b4aa4bf966a7b6d9e89ddbd44
Peter Admilson Ramaldes <peter.ramaldes@gmail.com>
2025-03-22 07:38:34 -0300

    Same for totalAmount

--- cap1/01-first-example/index.js
+++ cap1/01-first-example/index.js
@@ -8,24 +8,30 @@ function main() {
 }
 
 function statement(invoice, plays) {
-  let totalAmount = 0;
   let result = `Statement for ${invoice.customer}\n`
 
   for (let perf of invoice.performances) {
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
-    totalAmount += amountFor(perf);
   }
 
-  result += `Amount owned is ${usd(totalAmount)}\n`;
+  result += `Amount owned is ${usd(totalAmount())}\n`;
   result += `You earned ${totalVolumeCredits()} credits\n`;
   return result;
 
-  function totalVolumeCredits() {
-    let volumeCredits = 0;
+  function totalAmount() {
+    let result = 0;
     for (let perf of invoice.performances) {
-      volumeCredits += volumeCreditsFor(perf);
+      result += amountFor(perf);
     }
-    return volumeCredits;
+    return result;
+  }
+
+  function totalVolumeCredits() {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += volumeCreditsFor(perf);
+    }
+    return result;
   }
 
   function playFor(aPerformance) {
