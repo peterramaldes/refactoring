41f18f9083f0069ed417fb6870fab2017a946bd7
Peter Admilson Ramaldes <peter.ramaldes@gmail.com>
2025-03-22 07:26:40 -0300

    Extract Function (USD) + Change Function Declaration

--- cap1/01-first-example/index.js
+++ cap1/01-first-example/index.js
@@ -11,18 +11,16 @@ function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`
-  const format = new Intl.NumberFormat("en-US",
-    { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format;
 
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
 
     // Show the line for this request
-    result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
+    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     totalAmount += amountFor(perf);
   }
 
-  result += `Amount owned is ${format(totalAmount / 100)}\n`;
+  result += `Amount owned is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
 
@@ -58,6 +56,13 @@ function statement(invoice, plays) {
     if ("comedy" === playFor(aPerformance).type)
       result += Math.floor(aPerformance.audience / 5);
     return result;
+  }
+
+  function usd(aNumber) {
+    return new Intl.NumberFormat(
+      "en-US",
+      { style: "currency", currency: "USD", minimumFractionDigits: 2 }
+    ).format(aNumber / 100);
   }
 }
 
