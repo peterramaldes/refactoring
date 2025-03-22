a518d91628c78901897835f62bc9cb190094d79a
Peter Admilson Ramaldes <peter.ramaldes@gmail.com>
2025-03-22 07:53:11 -0300

    Extract Render Plain Text

--- cap1/01-first-example/index.js
+++ cap1/01-first-example/index.js
@@ -8,6 +8,10 @@ function main() {
 }
 
 function statement(invoice, plays) {
+  return renderPlainText(invoice, plays);
+}
+
+function renderPlainText(invoice, plays) {
   let result = `Statement for ${invoice.customer}\n`
 
   for (let perf of invoice.performances) {
