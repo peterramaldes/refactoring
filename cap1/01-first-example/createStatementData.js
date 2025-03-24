0ce8bce6828e247ca2f8b85d6bf15f3e9746fd81
Peter Admilson Ramaldes <peter.ramaldes@gmail.com>
2025-03-24 07:36:40 -0300

    Inline Function

--- cap1/01-first-example/createStatementData.js
+++ cap1/01-first-example/createStatementData.js
@@ -41,7 +41,7 @@ function createStatementData(invoice, plays) {
     const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
     const result = Object.assign({}, aPerformance);
     result.play = calculator.play;
-    result.amount = amountFor(result);
+    result.amount = calculator.amount;
     result.volumeCredits = volumeCreditsFor(result);
     return result;
   }
