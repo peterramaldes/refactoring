86b63dc1a5cd2cb1db0efa43941f05d3991df1fe
Peter Admilson Ramaldes <peter.ramaldes@gmail.com>
2025-03-26 05:40:54 -0300

    Replace Conditional with Polymorphism

--- cap1/01-first-example/createStatementData.js
+++ cap1/01-first-example/createStatementData.js
@@ -7,25 +7,7 @@ class PerformanceCalculator {
   }
 
   get amount() {
-    let result = 0;
-    switch (this.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (this.performance.audience > 30) {
-          result += 1000 * (this.performance.audience - 30)
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (this.performance.audience > 20) {
-          result += 10000 + 500 * (this.performance.audience - 20);
-        }
-        result += 300 * this.performance.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${this.play.type}`)
-    }
-    return result;
+    throw new Error('subclass responsability');
   }
 
   get volumeCredits() {
@@ -37,6 +19,36 @@ class PerformanceCalculator {
 
 }
 
+class TragedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 0;
+    result = 40000;
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30)
+    }
+    return result;
+  }
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 30000;
+    if (this.performance.audience > 20) {
+      result += 10000 + 500 * (this.performance.audience - 20);
+    }
+    result += 300 * this.performance.audience;
+    return result;
+  }
+}
+
+function createPerformanceCalculator(aPerformance, play) {
+  switch (play.type) {
+    case "tragedy": return new TragedyCalculator(aPerformance, play);
+    case "comedy": return new ComedyCalculator(aPerformance, play);
+    default: throw new Error(`unknown type: ${play.type}`);
+  }
+}
+
 function createStatementData(invoice, plays) {
   const result = {};
   result.customer = invoice.customer;
@@ -46,14 +58,13 @@ function createStatementData(invoice, plays) {
   return result;
 
   function enrichPerformance(aPerformance) {
-    const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
+    const calculator = createPerformanceCalculator(aPerformance, playFor(aPerformance));
     const result = Object.assign({}, aPerformance);
     result.play = calculator.play;
     result.amount = calculator.amount;
     result.volumeCredits = calculator.volumeCredits;
     return result;
   }
-
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
